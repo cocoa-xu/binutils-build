bdf07cb33f23ae0dd454454b080c03e02577798e
Cocoa <i@uwucocoa.moe>
2023-09-22 22:17:42 +0800

    ci

--- compile_binutils.sh
+++ compile_binutils.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
-ARCH=$1
-if [ -z "${ARCH}" ]; then
+TARGET_ARCH=$1
+if [ -z "${TARGET_ARCH}" ]; then
     echo "Error: Please pass the desired arch, i.e., i386, x86_64, aarch64"
     exit 1
 fi
-if [[ "$ARCH" == *"-"* ]]; then
-  FULL_TRIPLET="${ARCH}"
+if [[ "$TARGET_ARCH" == *"-"* ]]; then
+  FULL_TRIPLET="${TARGET_ARCH}"
 else
-  FULL_TRIPLET="${ARCH}-unknown-linux-gnu"
+  FULL_TRIPLET="${TARGET_ARCH}-unknown-linux-gnu"
 fi
 echo "[+] build for ${FULL_TRIPLET}"
 
 BINUTILS_VERSION="${2:-2.38}"
 echo "[+] using binutils ${BINUTILS_VERSION}"
 
-MAKE="${MAKE:-make}"
+UNAME_M=$(uname -m)
+HOST_ARCH="${3:-$UNAME_M}"
+HOST_SYSTEM=$4
+HOST_TRIPLET="${HOST_ARCH}-${HOST_SYSTEM}"
 
-BINUTILS_DIR="${HOME}/.cache/binutils"
+MAKE="${MAKE:-make}"
+BINUTILS_DIR_DEFAULT="${HOME}/.cache/binutils"
+BINUTILS_DIR=${BINUTILS_DIR:-$BINUTILS_DIR_DEFAULT}
 BINUTILS_SRC="${BINUTILS_DIR}/binutils-${BINUTILS_VERSION}"
-BINUTILS_INSTALL_DIR="${BINUTILS_DIR}/${FULL_TRIPLET}-${BINUTILS_VERSION}"
+BINUTILS_INSTALL_DIR="${BINUTILS_DIR}/${HOST_TRIPLET}_${FULL_TRIPLET}_${BINUTILS_VERSION}"
 
 mkdir -p "${BINUTILS_DIR}"
 cd "${BINUTILS_DIR}"
@@ -31,6 +36,14 @@ rm -rf "${BINUTILS_INSTALL_DIR}"
 tar -xf "${BINUTILS_DIR}/binutils-${BINUTILS_VERSION}.tar.gz"
 mkdir -p "${BINUTILS_INSTALL_DIR}"
 cd "${BINUTILS_SRC}"
+
+UNAME_S=$(uname -s)
+case "${UNAME_S}" in
+    Darwin*)
+        export CFLAGS="-arch ${HOST_ARCH}" ;;
+    *)
+        ;;
+esac
 ./configure \
     --disable-debug \
     --disable-dependency-tracking \
@@ -40,7 +53,7 @@ cd "${BINUTILS_SRC}"
     --disable-multilib \
     --disable-nls \
     --disable-werror
-UNAME_S=$(uname -s)
+
 case "${UNAME_S}" in
     Darwin*)
         $MAKE -j`sysctl -n hw.ncpu` ;;
@@ -48,7 +61,6 @@ case "${UNAME_S}" in
         $MAKE -j`nproc` ;;
     *)
         $MAKE ;;
-esac
 esac
 $MAKE install && \
     rm -rf "${BINUTILS_INSTALL_DIR}/share" && \
